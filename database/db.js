c6b0134efc20cfd6ed3cbfea242be60e2fe031c6
Shubhangcs <cshubhanga@gmail.com>
2024-03-05 19:28:21 +0530

    h

--- database/db.js
+++ database/db.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
   host: process.env.db_host,
   user: process.env.db_user,
   password: process.env.db_password,
-  database: process.env.db_name
+  database: process.env.db_name,
+  port: 20055
 });
 
 connection.connect((err) => {
