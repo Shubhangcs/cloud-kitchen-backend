27359177f97840343423aac9c24f0dd2e0b34a49
Shubhangcs <cshubhanga@gmail.com>
2024-03-08 17:11:19 +0530

    done

--- controller/cart_query.js
+++ controller/cart_query.js
@@ -1,62 +1,49 @@
 const db = require("../database/db");
 
 module.exports = (req, res, next) => {
- try {
-     const { price, quantity, userid, foodid, date } = req.body;
-     const retriveData = `SELECT  * FROM CARTITEMS`;
-     db.query(retriveData, (error, result) => { 
-         if (error) {
-             res.status(500).json({ status: false, message: error.message });
-         }
-         for (var i = 0; i <= result.length; i++) {
-           if (
-             result[i]["USER_ID"] == userid &&
-             result[i]["FOOD_ID"] == foodid
-           ) {
-             const addDataQuery = `UPDATE CARTITEMS SET CART_PRICE = ? , CART_QUANTITY = ? WHERE USER_ID = ? AND FOOD_ID = ?`;
-             db.query(
-               addDataQuery,
-               [
-                 price + result[index]["CART_PRICE"],
-                 quantity + result[index]["CART_QUANTITY"],
-                 userid,
-                 foodid,
-               ],
-               (error, result) => {
-                 res
-                   .status(500)
-                   .json({ status: false, message: error.message });
-                 res
-                   .status(200)
-                   .json({
-                     status: true,
-                     message: "Query Execuited Successfully",
-                   });
-               }
-             );
-           } else {
-             const addToCartQuery = `INSERT INTO CARTITEMS (CART_PRICE , CART_QUANTITY , USER_ID , FOOD_ID , CART_DATE) VALUES(?,?,?,?,?)`;
-             db.query(
-               addToCartQuery,
-               [price, quantity, userid, foodid, date],
-               (error, result) => {
-                 if (error) {
+    try {
+        const { price, quantity, userid, foodid, date } = req.body;
+        const retriveData = `SELECT  * FROM CARTITEMS WHERE USER_ID = ?;`;
+        var finalQuery = ``;
+        var itemList = [];
+        db.query(retriveData, [userid], (error, result) => { 
+            if (error) { 
+                res.status(500).json({status:false , message:error.message});
+            }
+            if (result.length != 0) { 
+                for (var i = 0; i < result.length; i++) { 
+                    if (result[i]['USER_ID'] == userid && result[i]['FOOD_ID'] == foodid) {
+                        finalQuery = `UPDATE CARTITEMS SET CART_QUANTITY = ? , CART_PRICE = ? WHERE USER_ID = ? AND FOOD_ID = ?`;
+                        itemList = [quantity , price , userid , foodid];
+                    } else { 
+                        finalQuery = `INSERT INTO CARTITEMS(CART_DATE , CART_PRICE , CART_QUANTITY , USER_ID , FOOD_ID) VALUES (?,?,?,?,?)`;
+                        itemList = [date , price , quantity , userid , foodid];
+                    }
+                }
+                db.query(finalQuery, itemList, (error, message) => { 
+                    if (error) { 
+                        res.status(500).json({status:false , message:error.message});
+                    }
+                    res.status(200).json({status:true , messaqge:"Query execuited"});
+                });
+            }
+            if (result.length === 0) { 
+                finalQuery = `INSERT INTO CARTITEMS(CART_DATE , CART_PRICE , CART_QUANTITY , USER_ID , FOOD_ID) VALUES (?,?,?,?,?)`;
+                itemList = [date, price, quantity, userid, foodid];
+                 db.query(finalQuery, itemList, (error, message) => {
+                   if (error) {
+                     res
+                       .status(500)
+                       .json({ status: false, message: error.message });
+                   }
                    res
-                     .status(500)
-                     .json({ status: false, message: error.message });
-                 }
-                 res
-                   .status(200)
-                   .json({
-                     status: true,
-                     message: "Added to cart Successfully",
-                   });
-               }
-             );
-           }
-         }
-     });
- } catch (error) {
-     res.status(500).json({ error:error.message });
- }
-};
+                     .status(200)
+                     .json({ status: true, messaqge: "Query execuited" });
+                 });
+            }
+        },);
+    } catch (e) { 
+        console.log(e);
+    };
+
+}
