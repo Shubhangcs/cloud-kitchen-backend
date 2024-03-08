dacd4005bf4b1690952a195f9f8666b257df313c
Shubhangcs <cshubhanga@gmail.com>
2024-03-08 12:09:37 +0530

    done

--- controller/cart_query.js
+++ controller/cart_query.js
@@ -1,13 +1,39 @@
-const db = require('../database/db');
+const db = require("../database/db");
 
-
-module.exports = (req, res, next) => { 
-    const { price, quantity, userid, foodid ,date} = req.body;
-    const addToCartQuery = `INSERT INTO CARTITEMS (CART_PRICE , CART_QUANTITY , USER_ID , FOOD_ID , CART_DATE) VALUES(?,?,?,?,?)`;
-    db.query(addToCartQuery, [price, quantity, userid, foodid,date], (error, result) => { 
-        if(error){ 
-            res.status(500).json({status:false , message:error.message});
-        }
-        res.status(200).json({status:true , message:"Added to cart Successfully"});
-    });
+module.exports = (req, res, next) => {
+  const { price, quantity, userid, foodid, date } = req.body;
+  const retriveData = `SELECT  * FROM CARTITEMS`;
+  for (var i = 0; i < retriveData["data"].length; i++) {
+      if (
+          retriveData["data"][index]["USER_ID"] == userid &&
+          retriveData["data"][index]["FOOD_ID"] == foodid
+      ) {
+          const addDataQuery = `UPDATE CARTITEMS SET CART_PRICE = ? , CART_QUANTITY = ? WHERE USER_ID = ? AND FOOD_ID = ?`;
+          db.query(addDataQuery, [
+              price + retriveData["data"][index]["CART_PRICE"],
+              quantity + retriveData["data"][index]["CART_QUANTITY"],
+              userid,
+              foodid
+          ], (error, result) => {
+              res.status(500).json({ status: false, message: error.message });
+              res.status(200).json({ status: true, message: "Query Execuited Successfully" });
+          }
+          );
+      }
+      else { 
+          const addToCartQuery = `INSERT INTO CARTITEMS (CART_PRICE , CART_QUANTITY , USER_ID , FOOD_ID , CART_DATE) VALUES(?,?,?,?,?)`;
+          db.query(
+            addToCartQuery,
+            [price, quantity, userid, foodid, date],
+            (error, result) => {
+              if (error) {
+                res.status(500).json({ status: false, message: error.message });
+              }
+              res
+                .status(200)
+                .json({ status: true, message: "Added to cart Successfully" });
+            }
+          );
+      }
+  }
 };
